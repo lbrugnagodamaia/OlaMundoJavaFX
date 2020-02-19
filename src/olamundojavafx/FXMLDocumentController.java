0369b194ae4895dde0a74618e68aadee1387cfce
Lucas Brugnago da Maia <54671855+lbrugnagodamaia@users.noreply.github.com>
2020-02-19 19:59:41 -0300

    Update FXMLDocumentController.java

--- src/olamundojavafx/FXMLDocumentController.java
+++ src/olamundojavafx/FXMLDocumentController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package olamundojavafx;
 
 import java.net.URL;
@@ -13,10 +8,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-/**
- *
- * @author Pichau
- */
 public class FXMLDocumentController implements Initializable {
     
     @FXML
@@ -30,7 +21,6 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
     }    
     
 }
